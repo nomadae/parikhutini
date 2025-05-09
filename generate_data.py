f1aeca798d944588a3cd2db3d0776383b95f6f73
Gilberto Domínguez <gilbertocarlos.dominguez@gmail.com>
2025-05-09 00:30:10 -0600

    preparaciones para ambiente de Python

--- generate_data.py
+++ generate_data.py
@@ -1,4 +1,10 @@
 from dbfread import DBF
+import pandas as pd
 
-table = table = DBF('./ce/ce_ccl.dbf', load=True, encoding="utf-8")
-print(table.records[0])
+
+dbf = DBF('./data/ce/ce_ccl.dbf', load=True, encoding="utf-8")
+df = pd.DataFrame(iter(dbf))
+df_localized = df[df['LONGI']!=0]
+df_volcanoes = df[df['Nombre'].str.contains('V')]
+
+json_string = df_volcanoes["Nombre"].to_json()
