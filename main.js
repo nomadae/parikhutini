061bce090b5557e1db620ff98993c3a5cf92be5b
Gilberto Domínguez <gilbertocarlos.dominguez@gmail.com>
2025-05-08 19:33:08 -0600

    first marker

--- main.js
+++ main.js
@@ -7,7 +7,9 @@ import Attribution from 'ol/control/Attribution'
 import Feature from 'ol/Feature.js';
 import Point from 'ol/geom/Point.js';
 import apply from 'ol-mapbox-style';
-
+import {Vector} from 'ol/source'
+import VectorLayer from 'ol/layer/Vector';
+import { Icon, Style } from 'ol/style';
 
 
 useGeographic();
@@ -37,3 +39,22 @@ const initFeature = new Feature({
 });
 
 
+const layer = new VectorLayer({
+  zIndex: 1000,
+  source: new Vector({
+    features: [
+      new Feature({
+        geometry: new Point([-102.25131182429995, 19.494074355290678])
+      })
+    ]
+  }),
+  style: new Style({
+    image: new Icon({
+      anchor: [0.5, 1],
+      crossOrigin: 'anonymous',
+      src: 'icon.png'
+    })
+  })
+});
+
+map.addLayer(layer);
